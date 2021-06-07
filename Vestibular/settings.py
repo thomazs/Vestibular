bf83ebaafdc96bfdcabaa7cb541c3c1e2a3c7af6
Francisco Passos <passos.27@hotmail.com>
2021-06-07 17:19:42 -0500

    tynince

--- Vestibular/settings.py
+++ Vestibular/settings.py
@@ -64,12 +64,12 @@ WSGI_APPLICATION = 'Vestibular.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 if not os.getenv('USERDOMAIN') == 'DESKTOP-JO69D6U':
     PRODUCAO = True
